23c4121f6a6f4f0c2fd44e1dc147fc584ee0701e
yongxiangng <yongxiangng@gmail.com>
2021-10-06 11:36:35 +0800

    Create details fxml

---
+++ src/main/java/seedu/address/ui/PersonDetailsCard.java
@@ -0,0 +1,73 @@
+package seedu.address.ui;
+
+import java.util.Comparator;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Region;
+import seedu.address.model.person.Person;
+
+/**
+ * An UI component that displays the detailed information of a {@code Person}.
+ */
+public class PersonDetailsCard extends UiPart<Region> {
+
+    private static final String FXML = "PersonDetailsCard.fxml";
+
+    /**
+     * Note: Certain keywords such as "location" and "resources" are reserved keywords in JavaFX.
+     * As a consequence, UI elements' variable names cannot be set to such keywords
+     * or an exception will be thrown by JavaFX during runtime.
+     *
+     * @see <a href="https://github.com/se-edu/addressbook-level4/issues/336">The issue on AddressBook level 4</a>
+     */
+
+    public final Person person;
+
+    @FXML
+    private HBox cardPane;
+    @FXML
+    private Label name;
+    @FXML
+    private Label phone;
+    @FXML
+    private Label address;
+    @FXML
+    private Label email;
+    @FXML
+    private FlowPane tags;
+
+    /**
+     * Creates a {@code PersonDetailsCard} with the given {@code Person}.
+     */
+    public PersonDetailsCard(Person person) {
+        super(FXML);
+        this.person = person;
+        name.setText(person.getName().fullName);
+        phone.setText(person.getPhone().value);
+        address.setText(person.getAddress().value);
+        email.setText(person.getEmail().value);
+        person.getTags().stream()
+                .sorted(Comparator.comparing(tag -> tag.tagName))
+                .forEach(tag -> tags.getChildren().add(new Label(tag.tagName)));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        // short circuit if same object
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof PersonDetailsCard)) {
+            return false;
+        }
+
+        // state check
+        PersonDetailsCard card = (PersonDetailsCard) other;
+        return person.equals(card.person);
+    }
+}
