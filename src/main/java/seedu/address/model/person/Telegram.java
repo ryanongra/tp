7e042d97be3add4e7bccc996ce4808c8b07b9a9f
ryanongra <ryanongra@gmail.com>
2021-10-30 19:08:14 +0800

    Update tests

--- src/main/java/seedu/address/model/person/Telegram.java
+++ src/main/java/seedu/address/model/person/Telegram.java
@@ -9,8 +9,8 @@ import static seedu.address.commons.util.AppUtil.checkArgument;
  */
 public class Telegram {
 
-    public static final String MESSAGE_CONSTRAINTS = "Telegram handles can take any values, and it should not be " +
-            "blank or contain any whitespace.";
+    public static final String MESSAGE_CONSTRAINTS = "Telegram handles can take any values, and it should not be "
+            + "blank or contain any whitespace.";
 
     /*
      * The first character of the address must not be a whitespace,
